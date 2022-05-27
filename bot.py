238dd90a52cae0925df4887fb8a293defb734676
FrostRay <frostray000@gmail.com>
2022-05-28 02:24:38 +0300

    Func_button_inline

--- bot.py
+++ bot.py
@@ -31,54 +31,75 @@ def send_message_welcome(message):
     else:
         bot.send_message(chat_id_bot, '!!!Всі кришки є!!!', reply_markup=markup)
 
+def action_menu_categories(message):
+    menu_change_categories = {'Кришки': 'cover', 'АКБ': 'akb', 'Скло': 'glass', 'Підсвітки': 'backlight', 'Сенсори': 'touch', 'Рамки': 'frame'}
+    markup = types.InlineKeyboardMarkup()
+    for key, value in menu_change_categories.items():
+        if message == key:
+            markup.add(types.InlineKeyboardButton('Взяти', callback_data=f'{value}_take'), types.InlineKeyboardButton('Знайти', callback_data=f'{value}_search'), types.InlineKeyboardButton('Добавити', callback_data=f'{value}_add'))
+            break
+    return markup
+
 
 @bot.message_handler(content_types=['text'])
 def some_func(message):
-    if message.text == 'Кришки':
-        markup = types.InlineKeyboardMarkup()
-        b1 = types.InlineKeyboardButton('Взяти', callback_data='cover_take')
-        b2 = types.InlineKeyboardButton('Знайти', callback_data='cover_search')
-        b3 = types.InlineKeyboardButton('Добавити', callback_data='cover_add')
-        markup.add(b1, b2, b3)
-        bot.send_message(message.chat.id, 'Що потрібно зробити з кришками?', reply_markup=markup)
-    else:
-        bot.send_message(message.chat.id, 'Шось не так')
+    bot.send_message(message.chat.id, f'{message.text}', reply_markup=action_menu_categories(message.text))
 
 def button_inine(call):
-    list_of_models = ['8', 'X', '11', '12']
+    dict_of_models_submodels = {
+        '5': ['5', '5s', 'se'],
+        '6': ['6', '6s', '6sPlus', '6Plus'],
+        #'7': ['7', '7Plus'],
+        #'8': ['8', '8Plus', 'se2020'],
+        #'X': ['X', 'Xs', 'XsM', 'XR'],
+        #'11': ['11', '11Pro', '11ProMax'],
+        #'12': ['12', '12Pro', '12ProMax', '12mini']
+    }
+
     markup = types.InlineKeyboardMarkup()
-    list_of_button = []
-    for value in list_of_models:
-        list_of_button.append(types.InlineKeyboardButton(value, callback_data=f'{call}_{value}')) 
-    return (markup, list_of_button) 
+    if call[-1] not in dict_of_models_submodels.keys():
+        st = ''
+        #якщо запит не має ключа з словника тоді виводити клавіатуру з моделями
+        for key in dict_of_models_submodels.keys():
+            st += f"types.InlineKeyboardButton('{key}', callback_data='{call}_{key}'),"
+        markup.add(eval(st))    
+
+    elif call[-1] in dict_of_models_submodels.keys():
+        #якщо ключ в запиті вказаний тоді виводити підмоделі
+        pass
+    else:
+        print('Помилка')
+
+    return markup
 
 @bot.callback_query_handler(func=lambda call: True)
 def handler_mes(call):
-    if call.data == 'cover_take':
-        tupple_of_function = button_inine(call.data)  
-        tupple_of_function[0].row(tupple_of_function[1][0], tupple_of_function[1][1], tupple_of_function[1][2], tupple_of_function[1][3])
-        bot.edit_message_text('Вибір моделі', call.message.chat.id, message_id=call.message.message_id, reply_markup=tupple_of_function[0])
-
-    elif call.data == 'cover_search':
-        markup = types.InlineKeyboardMarkup()
-        b1 = types.InlineKeyboardButton('8', callback_data=f'{call.data}_8')
-        b2 = types.InlineKeyboardButton('X', callback_data=f'{call.data}_X')
-        b3 = types.InlineKeyboardButton('11', callback_data=f'{call.data}_11')
-        b4 = types.InlineKeyboardButton('12', callback_data=f'{call.data}_12')
-        markup.row(b1, b2, b3, b4)
-        bot.edit_message_text('Вибір моделі', call.message.chat.id, message_id=call.message.message_id, reply_markup=markup)
-
-    elif call.data == 'cover_add':
-        markup = types.InlineKeyboardMarkup()
-        b1 = types.InlineKeyboardButton('8', callback_data=f'{call.data}_8')
-        b2 = types.InlineKeyboardButton('X', callback_data=f'{call.data}_X')
-        b3 = types.InlineKeyboardButton('11', callback_data=f'{call.data}_11')
-        b4 = types.InlineKeyboardButton('12', callback_data=f'{call.data}_12')
-        markup.row(b1, b2, b3, b4)
-        bot.edit_message_text('Вибір моделі', call.message.chat.id, message_id=call.message.message_id, reply_markup=markup)
+    if len(call.data.split('_')) == 3:
+        bot.edit_message_text('Що робим далі?', call.message.chat.id, message_id=call.message.message_id) 
+    else:
+        print(call.data.split('_'))
+        bot.edit_message_text('Вибір моделі', call.message.chat.id, message_id=call.message.message_id, reply_markup=button_inine(call.data))
     
-    elif call.data == 'cover_take_8':
-        pass
+    # elif call.data == 'cover_search':
+    #     markup = types.InlineKeyboardMarkup()
+    #     b1 = types.InlineKeyboardButton('8', callback_data=f'{call.data}_8')
+    #     b2 = types.InlineKeyboardButton('X', callback_data=f'{call.data}_X')
+    #     b3 = types.InlineKeyboardButton('11', callback_data=f'{call.data}_11')
+    #     b4 = types.InlineKeyboardButton('12', callback_data=f'{call.data}_12')
+    #     markup.row(b1, b2, b3, b4)
+    #     bot.edit_message_text('Вибір моделі', call.message.chat.id, message_id=call.message.message_id, reply_markup=markup)
+
+    # elif call.data == 'cover_add':
+    #     markup = types.InlineKeyboardMarkup()
+    #     b1 = types.InlineKeyboardButton('8', callback_data=f'{call.data}_8')
+    #     b2 = types.InlineKeyboardButton('X', callback_data=f'{call.data}_X')
+    #     b3 = types.InlineKeyboardButton('11', callback_data=f'{call.data}_11')
+    #     b4 = types.InlineKeyboardButton('12', callback_data=f'{call.data}_12')
+    #     markup.row(b1, b2, b3, b4)
+    #     bot.edit_message_text('Вибір моделі', call.message.chat.id, message_id=call.message.message_id, reply_markup=markup)
+    
+    # elif call.data == 'cover_take_8':
+    #     pass
 
 
 
