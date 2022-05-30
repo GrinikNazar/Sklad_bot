a0f723b7b833a8ba181d97850dfcedfb95b10793
FrostRay <frostray000@gmail.com>
2022-05-30 08:22:20 +0300

    color OK

--- bot.py
+++ bot.py
@@ -81,15 +81,8 @@ def button_inine(call):
         'white': []
     }
 
-    #Exapmle
-    #cover_take_8_8Plus_black
-    #len() = 5
-    if call.split('_')[0] == 'cover' or call.split('_')[0] == 'touch' or call.split('_')[0] == 'glass':
-        len_request = 5
-    else:
-        len_request = 4
-
     len_call = len(call.split('_')) 
+
     list_of_button = []
     markup = types.InlineKeyboardMarkup()
 
@@ -103,93 +96,33 @@ def button_inine(call):
         #якщо ключ в запиті вказаний тоді виводити підмоделі
         for key, values in dict_of_models_submodels.items():
             if key == call.split('_')[-1]:
-                for but in values:
-                    list_of_button.append(types.InlineKeyboardButton(f'{but}', callback_data=f'{call}_{but}'))
-                break
+                if call.split('_')[0] in model_class.keys():
+                    for but in values:
+                        list_of_button.append(types.InlineKeyboardButton(f'{but}', callback_data=f'{call}_{but}'))
+                    break
+                else:
+                    for but in values:
+                        list_of_button.append(types.InlineKeyboardButton(f'{but}', callback_data=f'{call}_{but}_{but}'))
+                    break
         markup.row(*list_of_button)
 
     #Добавити кольори
-    elif len_request == 5:
+    elif call.split('_')[0] in model_class.keys() and len_call == 4:
         if call.split('_')[-1] in model_class['cover']:
             for color in cover_color_choise:
                 if call.split('_')[-1] in color[0]:
-                    for color_mod in color[1]:
-                        list_of_button.append(types.InlineKeyboardButton(f'{color_mod.title()}', callback_data=f'{call}_{color_mod}'))
+                    markup.row(*[types.InlineKeyboardButton(f'{color_mod.title()}', callback_data=f'{call}_{color_mod}') for color_mod in color[1]])
                     break
-            markup.row(*list_of_button)
-        return markup
-    else:
-        #видати заит
-        pass
 
     return markup
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def handler_mes(call):
     if len(call.data.split('_')) == 5:
-        bot.edit_message_text(button_inine(call.data), call.message.chat.id, message_id=call.message.message_id) 
+        bot.edit_message_text(call.data, call.message.chat.id, message_id=call.message.message_id)
     else:
-        print(call.data.split('_'))
         bot.edit_message_text('Вибір моделі', call.message.chat.id, message_id=call.message.message_id, reply_markup=button_inine(call.data))
-    
-    # elif call.data == 'cover_search':
-    #     markup = types.InlineKeyboardMarkup()
-    #     b1 = types.InlineKeyboardButton('8', callback_data=f'{call.data}_8')
-    #     b2 = types.InlineKeyboardButton('X', callback_data=f'{call.data}_X')
-    #     b3 = types.InlineKeyboardButton('11', callback_data=f'{call.data}_11')
-    #     b4 = types.InlineKeyboardButton('12', callback_data=f'{call.data}_12')
-    #     markup.row(b1, b2, b3, b4)
-    #     bot.edit_message_text('Вибір моделі', call.message.chat.id, message_id=call.message.message_id, reply_markup=markup)
-
-    # elif call.data == 'cover_add':
-    #     markup = types.InlineKeyboardMarkup()
-    #     b1 = types.InlineKeyboardButton('8', callback_data=f'{call.data}_8')
-    #     b2 = types.InlineKeyboardButton('X', callback_data=f'{call.data}_X')
-    #     b3 = types.InlineKeyboardButton('11', callback_data=f'{call.data}_11')
-    #     b4 = types.InlineKeyboardButton('12', callback_data=f'{call.data}_12')
-    #     markup.row(b1, b2, b3, b4)
-    #     bot.edit_message_text('Вибір моделі', call.message.chat.id, message_id=call.message.message_id, reply_markup=markup)
-    
-    # elif call.data == 'cover_take_8':
-    #     pass
-
-
-
-#Приклад обробника
-# @bot.callback_query_handler(func=lambda call: True)
-# def handle(call):
-#     if call.data == 'cover':
-#         bot.send_message(call.message.chat.id, 'Які дії з кришками ви хочете зробити?')
-#         bot.answer_callback_query(call.id)
-
-
-# @bot.message_handler(content_types='text')
-# def get_cover(message):
-#     chat_id_bot = 375385945
-#     chat_id_chat = -674239373
-#     if message.text == f'@{bot.get_me().username} q':
-#         bot.send_message(chat_id_bot, 'О здоров)')
-#     elif message.text[:3] == 'add':
-#         list_of_addicted = message.text[4:].split('\n')
-#         gspread_my_py.add_cover(list_of_addicted)
-#         bot.send_message(chat_id_bot, 'Кришки добавлені')
-
-#     elif message.text[:-3:-1] in [str(x) + ' ' for x in range(1, 20)]:
-#         try:
-#             user = message.chat.first_name
-#             mes = gspread_my_py.get_cover(message.text)
-#             bot.send_message(chat_id_bot, f'{user} {mes[0]}\n{mes[1]}')
-#             bot.send_message(chat_id_chat, f'{user} {mes[0]}\n{mes[1]}')
-#         except ValueError:
-#             bot.reply_to(message, 'Вася, напиши нормально! Шо це за каляки маляки?')
-#         except UnboundLocalError:
-#             bot.reply_to(message, 'Вася, напиши нормально! Шо це за каляки маляки?')
-#     else:
-#         if gspread_my_py.search_covers(message.text):
-#             bot.reply_to(message, 'Вдалося знайти:')
-#             bot.send_message(message.chat.id, gspread_my_py.search_covers(message.text))
-#         else:
-#             bot.send_message(message.chat.id, 'Чувак, ти шось не то вводиш, подивись звідки руки!')
 
 
 bot.polling()
