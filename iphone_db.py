3a7c4964c12e1165e629ff097994fbd92f74c1e5
GrinikNazar <hrynyknazar@gmail.com>
2022-06-01 19:45:28 +0300

    db_v2

--- iphone_db.py
+++ iphone_db.py
@@ -3,13 +3,17 @@ import os
 
 db = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'iphone_parts.db'))
 
-c = db.cursor()
+cb = db.cursor()
 
-cover = 'cover'
-model = 'model'
+search = 'cover'
 
-r = c.execute('SELECT model FROM models WHERE (?) = 1', (cover,)).fetchall()
+# r = cb.execute('SELECT model FROM models WHERE akb = ? OR glass = ? OR backlight = ? OR touch = ? OR frame = ? OR cover = ?', (search, search, search, search, search, search)).fetchall()
+# print(r)
 
-print(r)
+model = '8'
 
+r = cb.execute('SELECT cover FROM colors WHERE model = ?', (model,)).fetchall()
+print(r[0][0])
+
+print(r[0][0].split('\r\n'))
 db.close()
