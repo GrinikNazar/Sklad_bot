bb0da7f9725b95005fb6adf2222b7e8768a1e328
FrostRay <frostray000@gmail.com>
2022-06-03 08:33:21 +0300

    add_back_v0

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from cgitb import text
 import gspread_my_py
 import iphone_db
 import conf
@@ -24,7 +25,6 @@ def send_message_welcome(message):
     markup.row(button_1)
 
     #Те що закінчилось
-    # chat_id_bot = 375385945
     if gspread_my_py.get_cover_null():
         bot.send_message(message.chat.id, '!!!Кришки які закінчились!!!', reply_markup=markup)
         string_of_covers_null = gspread_my_py.get_cover_null()
@@ -32,77 +32,71 @@ def send_message_welcome(message):
     else:
         bot.send_message(message.chat.id, '!!!Всі кришки є!!!', reply_markup=markup)
 
+#функція для створення клавіатури
 def action_menu_categories(message):
-    menu_change_categories = {'Кришки': 'cover', 'АКБ': 'akb', 'Скло': 'glass', 'Підсвітки': 'backlight', 'Сенсори': 'touch', 'Рамки': 'frame'}
+    change_categories = iphone_db.gen_keyboard(message)
     markup = types.InlineKeyboardMarkup()
-    for key, value in menu_change_categories.items():
-        if message == key:
-            markup.add(types.InlineKeyboardButton('Взяти', callback_data=f'{value}_take'), types.InlineKeyboardButton('Знайти', callback_data=f'{value}_search'), types.InlineKeyboardButton('Добавити', callback_data=f'{value}_add'))
-            break
+    markup.add(types.InlineKeyboardButton('Взяти', callback_data=f'{change_categories}_take'), types.InlineKeyboardButton('Знайти', callback_data=f'{change_categories}_search'))
     return markup
 
 
 @bot.message_handler(content_types=['text'])
 def some_func(message):
+    global text_message
+    text_message = message.text
     bot.send_message(message.chat.id, f'{message.text}', reply_markup=action_menu_categories(message.text))
 
+#функція створення клавіатури для вибору моделей і кольорів
 def button_inine(call):
 
     markup = types.InlineKeyboardMarkup()
-
+    #добавити модель
     if len(call.split('_')) == 2:
         markup.row(*[types.InlineKeyboardButton(f'{key}', callback_data=f'{call}_{key}') for key in iphone_db.choise_models(call.split('_')[0])])
+        markup.row(types.InlineKeyboardButton('Назад', callback_data=f'{call}_back'))
 
-
+    #добавити підмодель
     elif len(call.split('_')) == 3:
-        markup.row(*[types.InlineKeyboardButton(f'{key}', callback_data=f'{call}_{key}') for key in iphone_db.choise_submodels(call.split('_')[0], call.split('_')[-1])])
-    # elif len(call.split('_')) == 3:
-    #     #якщо ключ в запиті вказаний тоді виводити підмоделі
-    #     for key, values in dict_of_models_submodels.items():
-    #         if key == call.split('_')[-1]:
-    #             if call.split('_')[0] in values[1]:
-    #                 for but in values[0]:
-    #                     list_of_button.append(types.InlineKeyboardButton(f'{but}', callback_data=f'{call}_{but}'))
-    #                 break
-    #             else:
-    #                 for but in values[0]:
-    #                     list_of_button.append(types.InlineKeyboardButton(f'{but}', callback_data=f'{call}_{but}_nocolor'))
-    #                 break
-    #     markup.row(*list_of_button)
+        if not iphone_db.choise_colors(call.split('_')[0], call.split('_')[-1]):
+            markup.row(*[types.InlineKeyboardButton(f'{key}', callback_data=f'{call}_{key}_nocolor') for key in iphone_db.choise_submodels(call.split('_')[0], call.split('_')[-1])])
+        else:
+            markup.row(*[types.InlineKeyboardButton(f'{key}', callback_data=f'{call}_{key}') for key in iphone_db.choise_submodels(call.split('_')[0], call.split('_')[-1])])
+        markup.row(types.InlineKeyboardButton('Назад', callback_data=f'{call}_back'))
 
     #Добавити кольори
     elif len(call.split('_')) == 4:
+        print(call.split('_'))
         string_color = iphone_db.choise_colors(call.split('_')[0], call.split('_')[-1])
-        if not string_color:
-            markup.row(*[types.InlineKeyboardButton(f'{color_mod.title()}', callback_data=f'{call}_{color_mod}') for color_mod in color[1]])
-    #     for color in cover_color_choise:
-    #         if call.split('_')[-1] in color[0]:
-    #             markup.row(*[types.InlineKeyboardButton(f'{color_mod.title()}', callback_data=f'{call}_{color_mod}') for color_mod in color[1]])
-    #             break
-    
+        if string_color:
+            string_color = string_color.split('\r\n')
+            markup.row(*[types.InlineKeyboardButton(f'{color_mod.title()}', callback_data=f'{call}_{color_mod}') for color_mod in string_color])
+            markup.row(types.InlineKeyboardButton('Назад', callback_data=f'{call}_back'))
+
     #добавити кількість
     elif len(call.split('_')) == 5:
         markup.row(types.InlineKeyboardButton(f'1', callback_data=f'{call}_1'))
         markup.add(*[types.InlineKeyboardButton(f'{i}', callback_data=f'{call}_{i}') for i in range(2, 11)])
+        markup.row(types.InlineKeyboardButton('Назад', callback_data=f'{call}_back'))
 
-
-
-    
+    #функція повертає клавіатуру в залежності від умов
     return markup
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def handler_mes(call):
-    # print(pre_reqest)
-    # if call.data.split('_')[-1] == 'back':
-    #     bot.edit_message_text('Вибір моделі', call.message.chat.id, message_id=call.message.message_id, reply_markup=button_inine(pre_reqest))
+    
+    if call.data.split('_')[-1] == 'back' and len(call.data.split('_')) == 3:
+        bot.edit_message_text(text_message, call.message.chat.id, message_id=call.message.message_id, reply_markup=action_menu_categories(text_message))
+    
+    elif call.data.split('_')[-1] == 'back':
+        bot.edit_message_text(text_message, call.message.chat.id, message_id=call.message.message_id, reply_markup=button_inine(('_').join(call.data.split('_')[:-2])))
 
-    if len(call.data.split('_')) == 6:
+    elif len(call.data.split('_')) == 6:
+        print(call.data.split('_'))
         bot.edit_message_text(call.data, call.message.chat.id, message_id=call.message.message_id)
 
     else:
-        # pre_reqest = call.data
-        # print(call.data.split('_')[0])
+        print(call.data.split('_'))
         bot.edit_message_text('Вибір моделі', call.message.chat.id, message_id=call.message.message_id, reply_markup=button_inine(call.data))
 
 bot.polling()
--- iphone_db.py
+++ iphone_db.py
@@ -10,18 +10,14 @@ with sqlite3. connect(os.path.join(os.path.dirname(__file__), 'iphone_parts.db')
         result = cb.execute('SELECT model FROM models WHERE akb = ? OR glass = ? OR backlight = ? OR touch = ? OR frame = ? OR cover = ?', (search, search, search, search, search, search)).fetchall()
         return [i[0] for i in result]
         
-# model = 'X'
+#Запит на другому кроці
     def choise_submodels(search, model):
         result = cb.execute(f'SELECT model FROM submodels WHERE akb = ? OR glass = ? OR backlight = ? OR touch = ? OR frame = ? OR cover = ?', (search, search, search, search, search, search,)).fetchall()
         return [i[0] for i in result if i[0][0] == model or i[0][:2] == model]
-# model = 'XsMax'
 
+#Запит для вибору кольору
     def choise_colors(search, model):
         return cb.execute(f'SELECT {search} FROM colors WHERE model = ?', (model,)).fetchall()[0][0]
         
-s = ''
-r = choise_colors('cover', '8Plus')
-print(r)
-if not r[0][0]:
-    s += 'nocolor'
-print(s)
+    def gen_keyboard(uk):
+        return cb.execute('SELECT eng FROM keyboard WHERE uk = ?', (uk,)).fetchone()[0]
