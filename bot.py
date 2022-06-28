f6997de272edb3a7ad338a39ad7f3b4254d6adf4
GrinikNazar <hrynyknazar@gmail.com>
2022-06-28 15:13:25 +0300

    con_error_v2

--- bot.py
+++ bot.py
@@ -177,5 +177,5 @@ if __name__ == '__main__':
     try:
         bot.polling(non_stop=True, timeout=600)
     except ConnectionError:
-        bot.send_message(375385945, 'Зламався')
         bot.polling(non_stop=True, timeout=600)
+        bot.send_message(375385945, 'Зламався')
